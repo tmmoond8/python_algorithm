69d2ff91b5172f626e7c1c4f563ae3d2d947b074
문태민 <tmmoon@synapsoft.co.kr>
2019-01-25 13:44:06 +0900

    - samename2, students2

---
+++ textbook/students2.py
@@ -0,0 +1,22 @@
+def students2(a):
+  d = dict()
+  for v in a:
+    keyValue = v.split(":")
+    d[int(keyValue[0].strip())] = keyValue[1].strip()
+  
+  def getStudent(num):
+    if(num in d):
+      return d[num]
+    return "?"
+  return getStudent
+input = ["39: Justin", "14: John", "67: Mike", "105: Summer"]
+
+getStudent = students2(input)
+print(getStudent(39))
+print(getStudent(14))
+print(getStudent(67))
+print(getStudent(22))
+
+
+
+
